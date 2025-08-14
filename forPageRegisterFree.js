c0dc6577a5c7726057cc1ee8fb0e7c8f7112e8d1
Hicham Chaabi <161089314+hichamchaabi04@users.noreply.github.com>
2025-08-14 13:00:15 +0100

    Delete forPageRegisterFree.js
    Delete forPageRegisterFree.js

--- forPageRegisterFree.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btnRetour = document.getElementById("btn-retour");
-    if (btnRetour) {
-        btnRetour.addEventListener("click", function () {
-            window.history.back();
-        });
-    }
-});
